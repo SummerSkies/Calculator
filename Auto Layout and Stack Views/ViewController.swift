6b1c7a148270501b991eee73cc455f4c1d681728
Juliana Nielson <nielsonjuliana@gmail.com>
2023-01-23 08:35:44 -0700

    Initial Commit

---
+++ Auto Layout and Stack Views/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Auto Layout and Stack Views
+//
+//  Created by Juliana Nielson on 1/23/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
